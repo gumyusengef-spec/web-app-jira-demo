d4fab9748f845b2f1bb2dd80ec30f909ce741eb3
GUMYUSENGE <gumyusengef@gmail.com>
2025-11-16 19:22:32 +0200

    Initial Node.js Express app setup

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
 app.get('/', (req, res) => {
-    res.send("Welcome to the new Jira-integrated homepage!");
+    res.send('Welcome to the Jira Integrated Node.js App');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
