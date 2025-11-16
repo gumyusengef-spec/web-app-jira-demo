fc964c31574a4e35ec455c8f6629cdf171b809eb
GUMYUSENGE <gumyusengef@gmail.com>
2025-11-16 19:51:57 +0200

    WEB-1: Add homepage route with welcome message

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the Jira Integrated Node.js App');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.get('/home', (req, res) => {
+    res.send('Welcome to the Homepage! This route is linked with Jira issue WEB-1.');
 });
 
